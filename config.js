68a959dfdf1ae8340e6640898e492224a4e9c753
fouad601 <fouadilfigo@gmail.com>
2025-04-07 22:12:26 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,9 @@ import { fileURLToPath } from 'url'
 global.botnumber = "" //Inserisci nelle apici il numero del bot (es. +393762030481)
 global.confirmCode = ""
 
-global.owner = [
+global.owner = [+66 840693119
+
+    
     ['393762030481', 'Proprietario', true],
 ] //Numero degli owner
 
